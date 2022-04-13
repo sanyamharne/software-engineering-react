58b82bab1da42efc5825853b85ee2acbfe9da346
“sanyamharne” <harnesanyam@gmail.com>
2022-04-13 14:38:51 -0400

    made chaneges to lgin and all test cases passed

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const TUITS_API = "https://cs5500-01-sp22.herokuapp.com/api/tuits";
-const USERS_API = "https://cs5500-01-sp22.herokuapp.com/api/users";
+const TUITS_API = "https://tuiter-fse.herokuapp.comapi/tuits";
+const USERS_API = "https://tuiter-fse.herokuapp.comapi/users";
 
 export const findAllTuits = () =>
     axios.get(TUITS_API)
